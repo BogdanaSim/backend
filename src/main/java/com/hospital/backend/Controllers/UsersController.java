dfc9d0899d1d7d377f6c22bcecc7759225315602
BogdanaSim <simionicabogdana@yahoo.com>
2023-05-12 04:01:50 +0300

    added update, delete user controller

--- src/main/java/com/hospital/backend/Controllers/UsersController.java
+++ src/main/java/com/hospital/backend/Controllers/UsersController.java
@@ -1,14 +1,14 @@
 package com.hospital.backend.Controllers;
 
 import com.hospital.backend.Converters.UserConverter;
+import com.hospital.backend.DTOs.ShiftDTO;
 import com.hospital.backend.DTOs.UserDTO;
+import com.hospital.backend.Models.Shift;
 import com.hospital.backend.Models.User;
 import com.hospital.backend.Services.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +24,17 @@ public class UsersController {
     public List<UserDTO> findAllUsers() {
         List<User> users = usersService.getAllUsers();
         return userConverter.convertModelListToDtoList(users);
+    }
+
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        User user = userConverter.convertDtoToModel(userDTO);
+        User updatedUser = usersService.update(user);
+        return userConverter.convertModelToDto(updatedUser);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteShift(@PathVariable Long id) {
+        usersService.deleteById(id);
     }
 }
